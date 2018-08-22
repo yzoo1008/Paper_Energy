fb2981b0f9231dcf55b60236b806c0d096260f90
KYJ <yzoo1008@snu.ac.kr>
2018-08-22 21:04:28 +0900

    나누기 0 방지

--- run.py
+++ run.py
@@ -17,8 +17,7 @@ check_step = 10
 def MinMaxScaler(data):
 	numerator = data - np.min(data, 0)
 	denominator = np.max(data, 0) - np.min(data, 0)
-	# noise term prevents the zero division
-	return numerator / (denominator)# + 1e-7)
+	return numerator / (denominator+ 1e-7)
 
 
 def DataGen(name):

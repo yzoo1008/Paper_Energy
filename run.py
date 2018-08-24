a503c105c6070c1510ee2220f1ffae0fccef187b
KYJ <yzoo1008@snu.ac.kr>
2018-08-24 23:01:51 +0900

    데이터 교체 run.py 완성

--- run.py
+++ run.py
@@ -2,19 +2,20 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import openpyxl
+import os
 
 
 tf.set_random_seed(777)
 
 # train Parameters
 seq_length = 7
-data_dim = 8
+data_dim = 5
 hidden_dim = 5
 output_dim = 1
 num_stacked_layers = 3
-learning_rate = 0.01
-num_epoch = 300
-check_step = 10
+learning_rate = 0.0005
+num_epoch = 10000
+check_step = 10000
 
 def MinMaxScaler(data):
 	numerator = data - np.min(data, 0)
@@ -24,22 +25,22 @@ def MinMaxScaler(data):
 def reverse(data, origin):
 	min = np.min(origin, 0)
 	max = np.max(origin, 0)
-	return data * (max[0] - min[0] + 1e-7) + min[0]
+	return data * (max[6] - min[6] + 1e-7) + min[6]
 
-def DataGen(name):
-	xy = np.load("./data/" + name + ".npy")
-	xy = MinMaxScaler(xy)
-	x = xy
-	y = xy[:, 0]
+def read(s, e):
 	dataX = []
 	dataY = []
-	for i in range(0, len(y) - seq_length):
-		_x = x[i: i + seq_length]
-		_y = y[i + seq_length]
-		dataX.append(_x)
-		dataY.append(_y)
+	for num in range(s, e+1):
+		xy = np.load("./data/" + str(num) + ".npy")
+		xy = MinMaxScaler(xy)
+		x = xy[:, [0, 3, 4, 5, 6]]
+		y = xy[:, 6]
+		for idx in range(0, len(y) - seq_length):
+			_x = x[idx: idx + seq_length]
+			_y = y[idx + seq_length]
+			dataX.append(_x)
+			dataY.append(_y)
 	return dataX, dataY
-
 
 def lstm_cell(ReLu = False):
 	if ReLu:
@@ -53,16 +54,19 @@ def rnn_cell(ReLu = False):
 	return tf.contrib.rnn.BasicRNNCell(num_units=hidden_dim, state_is_tuple=True, activation=tf.tanh)
 
 
-trainData = DataGen("train")
-testData = DataGen("test")
+trainData = read(2014, 2017)
+testData = read(2018, 2018)
 
 trainX, trainY = trainData[0], np.reshape(trainData[1], (-1, 1))
 testX, testY = testData[0], np.reshape(testData[1], (-1, 1))
 
+# print(np.shape(trainX), np.shape(trainY))
+# print(np.shape(testX), np.shape(testY))
+
 X = tf.placeholder(tf.float32, [None, seq_length, data_dim])
 Y = tf.placeholder(tf.float32, [None, 1])
 
-multi_cells = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in range(num_stacked_layers)], state_is_tuple=True)
+multi_cells = tf.contrib.rnn.MultiRNNCell([lstm_cell(True) for _ in range(num_stacked_layers)], state_is_tuple=True)
 outputs, _ = tf.nn.dynamic_rnn(multi_cells, X, dtype=tf.float32)
 Y_pred = tf.contrib.layers.fully_connected(outputs[:, -1], output_dim, activation_fn=None)
 
@@ -95,35 +99,44 @@ with tf.Session() as sess:
 	# plt.ylabel("Stock Price")
 	# plt.show()
 
-	tx = np.load("./data/test.npy")
-	testdataY = reverse(testY, tx)
+	test_xy = np.load("./data/2018.npy")
+	test_y = reverse(testY, test_xy)
 
-	sum = 0.
+	error_sum = 0.
+	max_diff = 0.
 	result = []
 	for i in range(len(testX)):
-		lastX = np.reshape(testX[i], (-1, seq_length, data_dim))
-		lastPredict = sess.run(Y_pred, feed_dict={X: lastX})
-		lastPredict = reverse(lastPredict, tx)
-		aa = abs(testdataY[i, 0] - lastPredict[0, 0]) / testdataY[i] * 100
-		sum = sum + aa
-		# print(lastPredict[0,0],testdataY[i],100-aa)
+		testX_one = np.reshape(testX[i], (-1, seq_length, data_dim))
+		test_y_pred = sess.run(Y_pred, feed_dict={X: testX_one})
+		test_y_pred = reverse(test_y_pred, test_xy)
+		aa = abs(test_y[i, 0] - test_y_pred[0, 0]) / test_y[i, 0] * 100
+		error_sum = error_sum + aa
+		if max_diff < abs(test_y[i, 0] - test_y_pred[0, 0]):
+			max_diff = abs(test_y[i, 0] - test_y_pred[0, 0])
+		result.append(test_y_pred[0, 0])
 
-		result.append(lastPredict[0, 0])
-	print("average: ", 100 - sum / len(testX))
+	print("average error: ", error_sum / len(testX), "%")
+	print("max diff: ", max_diff)
 
-	wr = openpyxl.load_workbook('./score.xlsx')
-	wrr = wr.active
+	if not "score.xlsx" in os.listdir("./"):
+		wb = openpyxl.Workbook()
+		ws = wb.active
+		wb.save("./score.xlsx")
+		wb.close()
 
-	wrr.cell(row=1, column=1).value = "Predict"
-	wrr.cell(row=1, column=2).value = "Truth"
+	wb = openpyxl.load_workbook('score.xlsx')
+	ws = wb.active
+
+	ws.cell(row=1, column=1).value = "Predict"
+	ws.cell(row=1, column=2).value = "Truth"
 	for i in range(1, len(testX) + 1):
-		wrr.cell(row=i + 1, column=1).value = result[i - 1]
-		wrr.cell(row=i + 1, column=2).value = testdataY[i - 1, 0]
+		ws.cell(row=i + 1, column=1).value = result[i - 1]
+		ws.cell(row=i + 1, column=2).value = test_y[i - 1, 0]
 
-	wr.save("./score.xlsx")
-	wr.close()
+	wb.save("./score.xlsx")
+	wb.close()
 
-	plt.plot(result)
-	plt.plot(testdataY)
-	plt.show()
+	# plt.plot(result)
+	# plt.plot(test_y)
+	# plt.show()
 

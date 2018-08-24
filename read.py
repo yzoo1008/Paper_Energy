327b7398735122109d68166ded92dde1f0ad1342
KYJ <yzoo1008@snu.ac.kr>
2018-08-24 18:14:48 +0900

    데이터 변경

--- read.py
+++ read.py
@@ -2,28 +2,26 @@ import numpy as np
 import openpyxl
 import os
 
-local = [[108, "서울"], [159, "부산"], [176, "대구"], [112, "인천"], [156, "광주"], [133, "대전"]]
-data = dict()
-for element in local:
-	data[element[0]] = []
-
-for year in range(2014, 2016):
-	wb = openpyxl.load_workbook('./기후/' + str(year) + '.xlsx', data_only=True)
-	sheet = wb.worksheets[0]
-	row_c = sheet.max_row
-	col_c = sheet.max_column
-	for row in range(2, row_c + 1):
-		key = sheet[row][0].value
-		frag = []
-		for col in range(2, 8):
-			frag.append(sheet[row][col].value)
-			data[key].append(frag)
-
-array = []
-for key in data.keys():
-	array.append(data[key])
 
 if not "data" in os.listdir("./"):
 	os.mkdir("data")
 
-np.save("./data/data.npy", array)
+wb = openpyxl.load_workbook('./data.xlsx', data_only=True)
+sheets = wb.worksheets
+
+for i in range(1, 6):
+	sheet = sheets[i]
+	data = []
+	row_c = sheet.max_row
+	col_c = sheet.max_column
+	for row in range(2, row_c + 1):
+		row_data = []
+		for col in range(0, 7):
+			row_data.append(sheet[row][col].value)
+		data.append(row_data)
+	np.save("./data/" + str(sheet.title) + ".npy", data)
+
+
+# for i in range(2014, 2019):
+# 	array = np.load("./data/" + str(i) + ".npy")
+# 	print(np.shape(array))
